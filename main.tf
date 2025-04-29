97886ed9c8ead390adf232e76247e461a7e7f8cb
mehziya <82148699+mehziya@users.noreply.github.com>
2025-04-29 19:07:37 +0530

    Update main.tf
    Signed-off-by: mehziya <82148699+mehziya@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -19,6 +19,15 @@ resource "aws_instance" "mysql_server"{
       Name = "mysql_server-Server"
   }
 }
+resource "aws_instance" "maven_server"{
+    ami="ami-084568db4383264d4"
+    instance_type = "t2.micro"
+    security_groups = ["sgalltraffic"]  # Reference the existing security group
+    key_name      = "mujahed"
+    tags = {
+      Name = "maven_server-Server"
+  }
+}
 
 output "tomcat_server_ip" {
   value = aws_instance.tomcat_server.public_ip
@@ -26,3 +35,5 @@ output "tomcat_server_ip" {
 output "mysql_server_ip" {
   value = aws_instance.mysql_server.public_ip
 }
+output "maven_server_ip" {
+  value = aws_instance.maven_server.public_ip
